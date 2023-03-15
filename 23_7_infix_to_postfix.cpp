09bf3edca8673ea2934957a500d3e1294e965f73
Purushottam Nawale <nawalepb19.mech@coep.ac.in>
2023-03-15 18:21:21 +0530

    commit

--- 23_7_infix_to_postfix.cpp
+++ 23_7_infix_to_postfix.cpp
@@ -46,7 +46,7 @@ string infixToPostfix(string s)
             }
             if (!st.empty())
             {
-                st.pop();
+                st.pop(); // This pop() is for removing '(' while checking the stack is empty of not
             }
         }
         else
@@ -85,5 +85,6 @@ If ^
     ‘^’ operator is right associative and other operators like ‘+’,’-‘,’*’ and ‘/’ are left-associative. Check especially for a condition when both,  operator at the top of the stack and the scanned operator are ‘^’. In this condition, the precedence of the scanned operator is higher due to its right associativity. So it will be pushed into the operator stack. In all the other cases when the top of the operator stack is the same as the scanned operator, then pop the operator from the stack because of left associativity due to which the scanned operator has less precedence.
 If operator
     If the precedence and associativity of the scanned operator are greater than the operator in the stack (or the stack is empty or the stack contains a '('),then push it.
-    Pop all the operators from the stack which are greater than or equal to in precedence than that of the scanned operator. After doing that Push the scanned operator to the stack. (If you encounter parenthesis while popping then stop there and push the scanned operator in the stack.)
-*/
+    If the precedence and associativity of the scanned operator are less than the operator in the stack then,
+        Pop and print all the operators from the stack which are greater than or equal to in precedence than that of the scanned operator. After doing that Push the scanned operator to the stack. (If you encounter parenthesis while popping then stop there and push the scanned operator in the stack.)
+    */
