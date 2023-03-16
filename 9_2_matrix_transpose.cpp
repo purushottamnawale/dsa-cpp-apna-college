4a6a1191beeb043faa635da9ce0c00e8ece7f223
Purushottam Nawale <nawalepb19.mech@coep.ac.in>
2023-03-16 18:30:20 +0530

    commit

--- 9_2_matrix_transpose.cpp
+++ 9_2_matrix_transpose.cpp
@@ -1,45 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // This program is only for square matrices.
 
-
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
 
     int A[n][n];
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<n;j++)
+        for (int j = 0; j < n; j++)
         {
-            cin>>A[i][j];
+            cin >> A[i][j];
         }
     }
 
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=i;j<n;j++)
+        for (int j = i; j < n; j++) // If j=0 used, then the program will do a transpose again, giving us the same matrix.
         {
-            //swap
-            int temp=A[i][j];
-            A[i][j]=A[j][i];
-            A[j][i]=temp;
+            // swap
+            int temp = A[i][j];
+            A[i][j] = A[j][i];
+            A[j][i] = temp;
         }
     }
 
+    // 00 01 02
+    //    11 12
+    //       22
 
-    cout<<"Matrix is : \n";
-    for(int i=0;i<n;i++)
+    cout << "Matrix is : \n";
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<n;j++)
+        for (int j = 0; j < n; j++)
         {
-            cout<<A[i][j]<<" ";
+            cout << A[i][j] << " ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
-
 
     return 0;
 }
