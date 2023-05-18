057ce6af1227e04331540d9b90985b92b54ca5f2
Purushottam Nawale <nawalepb19.mech@coep.ac.in>
2023-05-18 18:20:26 +0530

    commit

--- 6_3_hexadecimal_to_decimal_function.cpp
+++ 6_3_hexadecimal_to_decimal_function.cpp
@@ -1,35 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+Hexadecimal to Decimal Function
+1CF = 1*256 + 12*16 + 15*1 
+    = 463
 
-int hexadecimalToDecimal(string n){
-    int ans=0;
-    int x=1;
-    int s=n.size();
-    // n.size() gives number of characters in a string
+*/
 
-    for(int i=s-1; i>=0; i--)
+
+
+int hexadecimalToDecimal(string n)
+{
+    int ans = 0;
+    int x = 1;
+    int s = n.size(); // n.size() gives number of characters in a string
+
+    for (int i = s - 1; i >= 0; i--)
     {
-        if(n[i] >='0' && n[i]<='9'){
-            ans += x*(n[i]-'0');
+        if (n[i] >= '0' && n[i] <= '9')
+        {
+            ans += x * (n[i] - '0'); // (n[i]-'0') gives ASCII decimal value difference
         }
-        // (n[i]-'0') gives ASCII decimal value difference
-        else if(n[i] >='A' && n[i] <='F'){
-            ans += x*(n[i]-'A'+10);
+        else if (n[i] >= 'A' && n[i] <= 'F')
+        {
+            ans += x * (n[i] - 'A' + 10); // (n[i]-'A'+10) gives ASCII decimal value difference
         }
-        // (n[i]-'A'+10) gives ASCII decimal value difference
-
         x *= 16;
     }
     return ans;
 }
 
-
-int main(){
+int main()
+{
     string n;
-    cin>>n;
+    cin >> n;
 
-    cout<<hexadecimalToDecimal(n);
+    cout << hexadecimalToDecimal(n);
 
     return 0;
 }
