0351af6bc704e674ca68f855f280c166398e74ea
purushottamnawale <nawalepurushottam@gmail.com>
2023-06-07 23:10:59 +0530

    commit

--- 15_1_prime_factorization_using_sieve.cpp
+++ 15_1_prime_factorization_using_sieve.cpp
@@ -14,21 +14,21 @@ So, Prime factorization of 40 is 40=2*2*2*5, where all the factors are prime.
 void primefactor(int n)
 {
     int spf[n + 1] = {0}; // SPF = Smallest Prime Factor
-    
-    for (int i = 2; i <= n; i++) // Marking smallest prime factor for every number to be itself
+
+    for (int i = 2; i <= n; i++) // Marking smallest prime factor for every number to be itself or Initialize all numbers as prime
     {
         spf[i] = i;
     }
 
-    for (int i = 2; i*i<= n; i++) // Using Sieve, Replace all Numbers from 2 to N, with their Prime Factors
+    for (int i = 2; i * i <= n; i++) // Using Sieve, Replace all Numbers from 2 to N, with their Prime Factors
     {
-        if (spf[i] == i) // Checking if i is prime
+        if (spf[i] == i) // Checking if spf[i] is prime (i.e., its value is still i)
         {
-            for (int j = i * i; j <= n; j += i) // Markin SPF for all numbers divisible by i
+            for (int j = i * i; j <= n; j += i)   // Mark all multiples of i with i as their SPF
             {
-                if (spf[j] == j) // Marking spf[j] if it not previously marked
+                if (spf[j] == j) // Checking spf[j] if it is not previously marked
                 {
-                    spf[j] = i;
+                    spf[j] = i; // Marking i with its smallest prime factor
                 }
             }
         }
@@ -37,7 +37,7 @@ void primefactor(int n)
     while (n != 1)
     {
         cout << spf[n] << " "; // Printing Prime Factors
-        n = n / spf[n]; // Prime factorization by dividing by smallest prime factor at every step
+        n = n / spf[n];        // Prime factorization by dividing by smallest prime factor at every step
     }
 }
 
