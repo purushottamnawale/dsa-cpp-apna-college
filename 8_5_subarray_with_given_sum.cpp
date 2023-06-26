96ce0ffc85c7ff3c9439cea47ff67aba5a843157
purushottamnawale <nawalepurushottam@gmail.com>
2023-06-26 22:45:04 +0530

    commit

--- 8_5_subarray_with_given_sum.cpp
+++ 8_5_subarray_with_given_sum.cpp
@@ -1,49 +1,55 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n,s;
-    cin>>n>>s;
+/*
+Input:
+5 12
+1 2 3 7 5
+
+*/
+
+int main()
+{
+    int n, s;
+    cin >> n >> s;
 
     int a[n];
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        cin>>a[i];
+        cin >> a[i];
     }
 
+    int i = 0, j = 0, st = -1, en = -1, sum = 0; // i and j are pointers, st and en are starting and ending indices of subarray, sum is the sum of subarray
 
-    int i=0,j=0,st=-1,en=-1,sum=0;
-    while(j<n && (sum+a[j])<=s)
+    while (j < n && (sum + a[j]) <= s)
     {
-        sum+=a[j];
+        sum += a[j];
         j++;
     }
 
-
-    if(sum==s)
+    if (sum == s)
     {
-        cout<<i+1<<" "<<j;
+        cout << i + 1 << " " << j;
         return 0;
     }
 
-
-    while(j<n)
+    while (j < n)
     {
-        sum+=a[j];
-        while(sum>s)
+        sum += a[j];
+        while (sum > s)
         {
-            sum-=a[i];
-            i++;
+            sum -= a[i]; 
+            i++; // // Adjust the starting index of subarray if the sum exceeds s
         }
-        if(sum=s)
+        if (sum == s)
         {
-            st=i+1;
-            en=j+1;
+            st = i + 1;
+            en = j + 1;
             break;
         }
         j++;
     }
-    cout<<st<<" "<<en;
+    cout << st << " " << en << endl;
 
     return 0;
 }
