699f9b234a59d3dd0a4818a7c1b645e6358b3c5b
purushottamnawale <nawalepurushottam@gmail.com>
2023-07-03 20:19:19 +0530

    commit

--- 9_1_spiral_order_matrix_traversal.cpp
+++ 9_1_spiral_order_matrix_traversal.cpp
@@ -1,63 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 
 int main()
 {
-    int n,m;
-    cin>>n>>m;
+    int n, m;
+    cin >> n >> m;
 
     int a[n][m];
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<m;j++)
+        for (int j = 0; j < m; j++)
         {
-            cin>>a[i][j];
+            cin >> a[i][j];
         }
     }
 
-    //spiral order printing
-    int row_start=0,row_end=n-1,column_start=0,column_end=m-1;
+    // spiral order printing
+    int row_start = 0, row_end = n - 1, column_start = 0, column_end = m - 1;
 
-    while(row_start<=row_end && column_start<=column_end)
+    while (row_start <= row_end && column_start <= column_end)
     {
-        //for row_start
-        for(int col=column_start;col<=column_end;col++)
+        // for row_start
+        for (int col = column_start; col <= column_end; col++)
         {
-            cout<<a[row_start][col]<<" ";
+            cout << a[row_start][col] << " ";
         }
         row_start++;
 
-
-        //for column_end
-        for(int row=row_start;row<=row_end;row++)
+        // for column_end
+        for (int row = row_start; row <= row_end; row++)
         {
-            cout<<a[row][column_end]<<" ";
+            cout << a[row][column_end] << " ";
         }
         column_end--;
 
-
-        //for row_end
-        if(row_start<=row_end){  // Boundary condition: In the first step, we have increased the rowStart by 1, so there can be a chance that the matrix segment will get reduced to one row i.e. both top and bottom boundaries are the same. So we only print the bottom boundary if(rowStart <= rowEnd).
-            for(int col=column_end;col>=column_start;col--)
+        // for row_end
+        if (row_start <= row_end) // Boundary condition: In the first step, we have increased the rowStart by 1, so there can be a chance that the matrix segment will get reduced to one row i.e. both top and bottom boundaries are the same. So we only print the bottom boundary if(rowStart <= rowEnd).
+        {
+            for (int col = column_end; col >= column_start; col--)
             {
-                cout<<a[row_end][col]<<" ";
+                cout << a[row_end][col] << " ";
             }
             row_end--;
         }
 
-        
-        //for column_start
-        if(column_start<=column_end){  // Boundary condition: In the second step, we have reduced the colEnd by 1, so there can be a chance that the matrix segment will get reduced to one column i.e. both left and right boundaries are the same. So we only print the left boundary if(colStart <= colEnd).
-            for(int row=row_end;row>=row_start;row--)
+        // for column_start
+        if (column_start <= column_end) // Boundary condition: In the second step, we have reduced the colEnd by 1, so there can be a chance that the matrix segment will get reduced to one column i.e. both left and right boundaries are the same. So we only print the left boundary if(colStart <= colEnd).
+        {
+            for (int row = row_end; row >= row_start; row--)
             {
-                cout<<a[row][column_start]<<" ";
+                cout << a[row][column_start] << " ";
             }
             column_start++;
         }
     }
-    
-
 
     return 0;
 }
