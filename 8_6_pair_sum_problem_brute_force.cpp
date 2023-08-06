9036bf9040c890cbeb8be1b81a442552396c2577
purushottamnawale <nawalepurushottam@gmail.com>
2023-08-06 21:42:39 +0530

    Pair Sum Problem

--- 8_6_pair_sum_problem_brute_force.cpp
+++ 8_6_pair_sum_problem_brute_force.cpp
@@ -1,38 +1,60 @@
-#include<iostream>
-#include<climits>
+#include <iostream>
+#include <climits>
 using namespace std;
 
-bool pairsum(int a[],int n,int k)
+/*
+Pair Sum Problem - Brute Force O(N^2)
+
+Check if there exists two elements in an array such that their sum is equal to given k.
+
+Input:
+8
+2 4 7 11 14 16 20 21
+31
+
+Output:
+3 6
+1
+
+
+*/
+
+bool pairsum(int a[], int n, int k)
 {
-    for(int i=0;i<n-1;i++)
+    for (int i = 0; i < n - 1; i++)
     {
-        for(int j=i+1;j<n;j++)
+        for (int j = i + 1; j < n; j++)
         {
-            if(a[i]+a[j]==k)
+            if (a[i] + a[j] == k)
             {
-                cout<<i<<" "<<j<<endl;
+                cout << i << " " << j << endl;
                 return true;
-            }            
+            }
         }
     }
     return false;
 }
 
-
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
 
     int a[n];
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        cin>>a[i];
+        cin >> a[i];
     }
 
     int k;
-    cin>>k;
+    cin >> k;
 
-    cout<<pairsum(a,n,k);
+    cout << pairsum(a, n, k) << endl;
+
+    // int arr[] = {2, 4, 7, 11, 14, 16, 20, 21};
+    // int k = 31;
+    // cout << pairsum(arr, 8, k) << endl;
+
 
     return 0;
 }
--- 8_6_pair_sum_problem_optimized.cpp
+++ 8_6_pair_sum_problem_optimized.cpp
@@ -1,22 +1,38 @@
-#include<iostream>
-#include<climits>
+#include <iostream>
+#include <climits>
 using namespace std;
 
-//This program works only with sorted array.
+/*
+Pair Sum Problem - Optimized O(N):
 
-bool pairsum(int a[],int n,int k)
+Check if there exists two elements in an array such that their sum is equal to given k.
+This program works only with sorted array.
+
+Input:
+8
+2 4 7 11 14 16 20 21
+31
+
+Output:
+3 6
+1
+
+
+*/
+
+bool pairsum(int a[], int n, int k)
 {
-    int low=0;
-    int high=n-1;
+    int low = 0;
+    int high = n - 1;
 
-    while(low<high)
+    while (low < high)
     {
-        if(a[low]+a[high]==k)
+        if (a[low] + a[high] == k)
         {
-            cout<<low<<" "<<high<<endl;
+            cout << low << " " << high << endl;
             return true;
         }
-        else if(a[low]+a[high]>k)
+        else if (a[low] + a[high] > k)
         {
             high--;
         }
@@ -28,21 +44,25 @@ bool pairsum(int a[],int n,int k)
     return false;
 }
 
-
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
 
     int a[n];
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        cin>>a[i];
+        cin >> a[i];
     }
 
     int k;
-    cin>>k;
+    cin >> k;
 
-    cout<<pairsum(a,n,k);
+    cout << pairsum(a, n, k) << endl;
+
+    // int arr[] = {2, 4, 7, 11, 14, 16, 20, 21};
+    // int k = 31;
+    // cout << pairsum(arr, 8, k) << endl;
 
     return 0;
 }
