cd260fc6c1aedef3d1da97f45c942dfc057d03c0
purushottamnawale <nawalepurushottam@gmail.com>
2023-09-22 23:13:41 +0530

    Binary Search

--- searching/8_2_binary_search.cpp
+++ searching/8_2_binary_search.cpp
@@ -1,62 +1,72 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-// Binary search algorithm is used in a sorted array.
+// Binary search algorithm is used only in a sorted array.
 
-int binarySearch(int arr[],int n,int key){
-    int s=0;
-    int e=n; // e=n-1;
-    while(s<=e){
-        int mid=(s+e)/2;
+int binarySearch(int arr[], int n, int key)
+{
+    int s = 0;
+    int e = n; // e=n-1;
+    while (s <= e)
+    {
+        int mid = (s + e) / 2;
 
-        if(arr[mid]==key){
+        if (arr[mid] == key)
+        {
             return mid;
         }
-        else if(arr[mid]>key){
-            e=mid-1;
+        else if (arr[mid] > key)
+        {
+            e = mid - 1;
         }
-        else{
-            s=mid+1;
+        else
+        {
+            s = mid + 1;
         }
     }
     return -1;
 }
 
 /*
-find 6
-1 2 3 4 5 6
+Explanation of the above code:
+
+arr = 1 2 3 4 5 6
+binarySearch(6)
     s=0
     e=5
     mid=2
     s=3
     4 5 6
-        s=3
-        e=5
-        mid=4
-        s=5
-        6
-            s=5
-            e=5
-            mid=5
-            returns index
 
+    s=3
+    e=5
+    mid=4
+    s=5
+    6
+
+    s=5
+    e=5
+    mid=5
+    returns 5
 
 */
 
-int main(){
+int main()
+{
 
     int n;
-    cin>>n;
+    cin >> n;
 
     int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
 
     int key;
-    cin>>key;
+    cin >> key;
 
-    cout<<binarySearch(arr,n,key);
+    cout << binarySearch(arr, n, key);
 
     return 0;
 }
